feec535804f8e469923652a9011f14d43423cc70
Wilson Leite <wi_fil@yahoo.com.br>
2021-02-06 22:11:40 +0000

    refactoring

--- receipts/process_receipts.py
+++ receipts/process_receipts.py
@@ -2,6 +2,7 @@ import os
 import glob
 import json
 import shutil
+import math
 
 try:
     ## https://docs.python.org/3/library/os.html#os.mkdir
@@ -13,16 +14,19 @@ except OSError:
 receipts = glob.glob('./new/receipt-[0-9]*.json')
 subtotal = 0.0
 
+## or.. for path in glob.iglob('./new/receipt-[0-9]*.json')
 for path in receipts:
     with open(path) as f:
         ## https://docs.python.org/3/library/json.html
         content = json.load(f)
         subtotal += float(content["value"])
-    name = path.split("/")[-1]
-    destination = f"./processed/{name}"
+    #name = path.split("/")[-1]
+    #destination = f"./processed/{name}"
+    destination = path.replace('new', 'processed')
     ## https://docs.python.org/3/library/shutil.html
     shutil.move(path, destination)
     print(f"moved '{path}' to '{destination}'")
 
 print("Receipt subtotal: $%.2f" % subtotal)
-print(f"TESTE: Receipt subtotal: ${subtotal}")
+print(f"TESTE: Receipt subtotal: ${math.ceil(subtotal)}")
+print(f"TESTE: Receipt subtotal: ${math.floor(subtotal)}")
